2791c4eac868a2d6af91806c32e35bc049ceb82a
Horacio Lerda <horacio.lerda@gmail.com>
2022-05-23 23:54:15 -0300

    Ejercicio 1 Varaiables y operadores

---
+++ README.md
@@ -0,0 +1,5 @@
+Ejercicios de Javascript
+
+1er commit inicial
+
+2do commit, ejercicio de variables y operadores, en este ejercicio me familiaricé con algunos operadores de variabels string, pude encontrar todo lo que necesitaba en W3School y el resto pude realizarlo basado en lo visto en clase.
---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,8 @@
+/*Ejecicio de variables donde las palabras son concatenadas creando una 3er palabra*/
+let palabra1 = "planta", palabra2= "piedra"
+let palabra3 = palabra1.concat(palabra2)
+console.log(palabra3)
+
+/*Ejecicio de variables donde se cuenta la cantidad de letras de las palabras y se suman y muestra el resultado*/
+var numeroletras = palabra1.length + palabra2.length
+console.log(numeroletras)
